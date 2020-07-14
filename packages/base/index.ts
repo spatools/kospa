c4073dda53caa3f205b2da8ef336915bb589c1a1
Maxime LUCE <maxime@touchify.co>
2020-07-15 00:52:43 +0200

    feat(base): improve index exports

--- packages/base/index.ts
+++ packages/base/index.ts
@@ -1,8 +1,4 @@
-import * as _activator from "./activator";
-export import activator = _activator;
-
-import * as _composer from "./composer";
-export import composer = _composer;
-
-import * as _system from "./system";
-export import system = _system;
+export * as activator from "./activator";
+export * as composer from "./composer";
+export * as loader from "./loader";
+export * as system from "./system";
